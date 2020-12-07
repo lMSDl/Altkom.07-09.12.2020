9755aa3f392a6f41fb9122181abcc1ff43067f9f
Paweł Łęczycki <pawel@msd.info.pl>
2020-12-07 09:59:41 +0100

    KISS

---
+++ WPCSharp/DesignPrinciples/PaymentAccount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPCSharp.DesignPrinciples
+{
+    public class PaymentAccount
+    {
+        public PaymentAccount(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; }
+        public string CustomerName { get; set; }
+        public float Incomes { get; set; }
+        public float Outcomes { get; set; }
+        public float AllowedDebit { get; set; }
+
+        public bool HasDebit()
+        {
+            return Incomes - Outcomes < 0;
+        }
+    }
+
+    
+}
---
+++ WPCSharp/DesignPrinciples/PaymentService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPCSharp.DesignPrinciples
+{
+    public class PaymentService
+    {
+        private ICollection<PaymentAccount> Customers { get; } = new List<PaymentAccount> { new PaymentAccount(1), new PaymentAccount(2), new PaymentAccount(3), new PaymentAccount(4), new PaymentAccount(5) };
+
+        public bool DeleteCustomer(PaymentAccount customerAccount)
+        {
+            return Customers.Remove(customerAccount);
+        }
+
+        public PaymentAccount FindByAllowedDebit(float allowedDebit)
+        {
+            return Customers.SingleOrDefault(x => x.AllowedDebit == allowedDebit);
+        }
+
+        public bool Charge(int customerId, float amount)
+        {
+            var customer = Customers.SingleOrDefault(x => x.Id == customerId);
+            if (customer == null)
+            {
+                return false;
+            }
+
+            if (customer.Incomes - customer.Outcomes + customer.AllowedDebit < amount)
+            {
+                return false;
+            }
+
+            customer.Outcomes += amount;
+            return true;
+        }
+
+        public void Fund(int customerId, float amount)
+        {
+            var customer = Customers.Where(x => x.Id == customerId).SingleOrDefault();
+            if (customer == null)
+            {
+                return;
+            }
+
+            customer.Incomes += amount;
+        }
+
+        public float? GetBalance(int customerId)
+        {
+            var customer = Customers.Where(x => x.Id == customerId).SingleOrDefault();
+            return customer?.Incomes - customer?.Outcomes;
+        }
+    }
+}
