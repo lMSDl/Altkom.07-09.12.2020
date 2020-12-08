f14e502124a9afe813c4a1962e8cd70c24b80b5b
Paweł Łęczycki <pawel@msd.info.pl>
2020-12-08 11:19:34 +0100

    composite

--- WPCSharp/Program.cs
+++ WPCSharp/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using WPCSharp.DesignPatterns.Structural.Bridge.III;
+using WPCSharp.DesignPatterns.Structural.Composite;
 
 namespace WPCSharp
 {
