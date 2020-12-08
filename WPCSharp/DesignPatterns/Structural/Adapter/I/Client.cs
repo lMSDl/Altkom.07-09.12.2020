5f583026a79f645983f25f0f67682053b043067d
Paweł Łęczycki <pawel@msd.info.pl>
2020-12-08 09:54:31 +0100

    Adapter

---
+++ WPCSharp/DesignPatterns/Structural/Adapter/I/Client.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WPCSharp.DesignPatterns.Creational.Builder;
+using WPCSharp.DesignPatterns.Creational.FactoryMethod;
+
+namespace WPCSharp.DesignPatterns.Structural.Adapter.I
+{
+    public static class Client
+    {
+        public static void Execute()
+        {
+            var circle = new Circle() { Radius = 5 };
+            var square = new Square() { Width = 8 };
+
+            Console.WriteLine(Check(circle));
+            Console.WriteLine(Check(new SquareToCircleAdapter(square)));
+        }
+
+        public static bool Check(ICircle circle)
+        {
+            return circle.Radius <= 5;
+        }
+    }
+}
--- WPCSharp/Program.cs
+++ WPCSharp/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using WPCSharp.DesignPatterns.Creational.ObjectPool;
+using WPCSharp.DesignPatterns.Structural.Adapter.I;
 
 namespace WPCSharp
 {
