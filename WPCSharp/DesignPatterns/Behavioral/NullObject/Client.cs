4d7b6f17e7f6533be02a16baffce3e10eabecf9a
Paweł Łęczycki <pawel@msd.info.pl>
2020-12-08 15:59:01 +0100

    null object

---
+++ WPCSharp/DesignPatterns/Behavioral/NullObject/Client.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPCSharp.DesignPatterns.Behavioral.NullObject
+{
+    public class Client
+    {
+        public static Output Output { get; set; } = new NullOutput();
+
+        public static void Execute()
+        {
+            while(true)
+            {
+                switch(Console.ReadKey().KeyChar)
+                {
+                    case 'c':
+                        Output = new ConsoleOutput();
+                        break;
+                    case 'd':
+                        Output = new DebugOutput();
+                        break;
+                    default:
+                        Output = new NullOutput();
+                        break;
+                }
+                Output.Show(Console.ReadLine());
+            }
+        }
+    }
+}
---
+++ WPCSharp/DesignPatterns/Behavioral/NullObject/NullOutput.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPCSharp.DesignPatterns.Behavioral.NullObject
+{
+    public class NullOutput : Output
+    {
+        public override void Show(string message)
+        {
+        }
+    }
+}
