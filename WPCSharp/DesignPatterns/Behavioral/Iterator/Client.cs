63ee760a4dc06f1472627b49fbe0eb0e3a0352a3
Paweł Łęczycki <pawel@msd.info.pl>
2020-12-09 09:39:36 +0100

    Iterator

---
+++ WPCSharp/DesignPatterns/Behavioral/Iterator/Client.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPCSharp.DesignPatterns.Behavioral.NullObject;
+
+namespace WPCSharp.DesignPatterns.Behavioral.Iterator
+{
+    public class Client
+    {
+        public static void Execute()
+        {
+            var list = new List<string> { "a", "b", "c", "d", "e" };
+
+            var buffer = new Buffer<string>(list);
+
+            var list2 = new List<string>();
+
+            /*var iterator = buffer.GetEnumerator();
+            while(iterator.MoveNext())
+            {
+                var item = iterator.Current;
+
+            }*/
+
+            foreach (var item in buffer)
+            {
+                var x = item.Item1 + item.Item2;
+                Console.WriteLine(x);
+                list2.Add(x);
+            }
+
+            foreach (var item in new Buffer<string>(list2))
+            {
+                var x = item.Item1 + item.Item2;
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
--- WPCSharp/Program.cs
+++ WPCSharp/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using WPCSharp.DesignPatterns.Behavioral.NullObject;
+using WPCSharp.DesignPatterns.Behavioral.Iterator;
 
 namespace WPCSharp
 {
